e94d8fd3b5d539bb06052ca6de842bfe12ae771f
Enrico Di Fazio <link@live.it>
2015-05-10 00:27:44 +0200

    Prova aggiunta commento - Enrico

--- app/src/main/java/it/polito/mobile/temporaryjobplacement/TemporaryJobPlacementApp.java
+++ app/src/main/java/it/polito/mobile/temporaryjobplacement/TemporaryJobPlacementApp.java
@@ -4,9 +4,6 @@ import android.app.Application;
 
 import com.parse.Parse;
 
-/**
- * Created by Nicolò on 21/04/2015.
- */
 public class TemporaryJobPlacementApp extends Application {
 
 public static int exitCode=2;
@@ -17,6 +14,8 @@ public static int exitCode=2;
 
         Parse.enableLocalDatastore(getApplicationContext());
         Parse.initialize(getApplicationContext(), "YfmdTSE9tXlIhIsnPZmZOjQMt8tu8nq7pXZ9mA0G", "Y2GtjfVYqbavbI4KeKvoBGM4EXq4Qs9wWfCzMpQS");
+
+        //PROVA GIT - blabla
 
         }
 }
