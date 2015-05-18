bb2a7b70716801b27754f183d5ef79315156ca83
s209019 <nico.fod@live.it>
2015-05-18 06:18:47 +0200

    languages

--- app/src/main/java/it/polito/mobile/temporaryjobplacement/model/Student.java
+++ app/src/main/java/it/polito/mobile/temporaryjobplacement/model/Student.java
@@ -67,7 +67,6 @@ public class Student extends ParseObject {
 
 
     public ArrayList<String> getLanguageSkills(){
-
         String languagesText=(String)this.getString("languages");
         if(languagesText==null || languagesText.trim().equals(""))return  new ArrayList<String>();
         String[] languages=languagesText.split("\n");
