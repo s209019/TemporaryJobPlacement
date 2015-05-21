5d5145c73be5584275b482eda6aa65c974e7999d
Enrico Di Fazio <link@live.it>
2015-05-21 21:23:45 +0200

    Classe riaggiunta XD

--- app/src/main/java/it/polito/mobile/temporaryjobplacement/pcompany/activities/OpenPositionsActivity.java
+++ app/src/main/java/it/polito/mobile/temporaryjobplacement/pcompany/activities/OpenPositionsActivity.java
@@ -1,5 +1,6 @@
 package it.polito.mobile.temporaryjobplacement.pcompany.activities;
 
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -7,8 +8,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+
 import it.polito.mobile.temporaryjobplacement.R;
+import it.polito.mobile.temporaryjobplacement.commons.utils.AccountManager;
+import it.polito.mobile.temporaryjobplacement.model.Application;
 import it.polito.mobile.temporaryjobplacement.pcompany.viewmanaging.DrawerManager;
+import it.polito.mobile.temporaryjobplacement.pstudent.activities.ApplicationDetailActivity;
 
 public class OpenPositionsActivity extends ActionBarActivity {
 
@@ -31,6 +38,23 @@ public class OpenPositionsActivity extends ActionBarActivity {
         drawerManager.setDrawer();
     }
 
+    public void onItemSelected(Application application) {
+
+        Intent detailIntent = new Intent(this, ApplicationDetailActivity.class);
+        detailIntent.putExtra("SELECTED_APP", application.getObjectId());
+        startActivity(detailIntent);
+
+    }
+/*
+    @Override
+    public void initializeProfile() throws Exception {
+        if(studentProfile==null)
+            studentProfile = AccountManager.getCurrentStudentProfile();
+
+
+    }
+*/
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -51,6 +75,43 @@ public class OpenPositionsActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+/*
+    public ParseQueryAdapter.QueryFactory<Application> getQueryFactory() throws Exception {
+        ParseQueryAdapter.QueryFactory<Application> query;
+        final Exception[] error={null};
+
+        query= new ParseQueryAdapter.QueryFactory<Application>() {
+
+            public ParseQuery<Application> create() {
+
+                ParseQuery<Application> query=null;
+                try {
+
+                    query = Application.getQuery();
+                    query.whereEqualTo("student", studentProfile);
+                    query.include("jobOffer");
+                    query.include("jobOffer.company");
+                    query.orderByDescending("createdAt");
+                    query.setLimit(100);
+                } catch (Exception e) {
+                    e.printStackTrace();error[0]=e;
+                }
+                return query;
+            }
+        };
+        if(error[0]!=null)throw error[0];
+        return query;
+    }
+*/
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        this.recreate();
+
+
+    }
+
 
     @Override
     public void onBackPressed(){
---
+++ app/src/main/java/it/polito/mobile/temporaryjobplacement/pcompany/viewmanaging/JobOfferQueryAdapter.java
@@ -0,0 +1,94 @@
+package it.polito.mobile.temporaryjobplacement.pcompany.viewmanaging;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.parse.ParseQueryAdapter;
+
+import it.polito.mobile.temporaryjobplacement.R;
+import it.polito.mobile.temporaryjobplacement.commons.utils.TimeManager;
+import it.polito.mobile.temporaryjobplacement.model.JobOffer;
+
+/**
+ * Created by Enrico on 11/05/15.
+ */
+public class JobOfferQueryAdapter extends ParseQueryAdapter<JobOffer> {
+
+    private InnerButtonManager innerButtonManager;
+    private int rowLayoutId;
+
+
+    public interface InnerButtonManager {
+        void configureButton(final JobOffer offer, final ImageButton innerButton);
+    }
+
+
+    public JobOfferQueryAdapter(Context context, QueryFactory<JobOffer> queryFactory, InnerButtonManager innerButtonManager, int rowLayoutId) {
+        super(context, queryFactory);
+        this.innerButtonManager=innerButtonManager;
+        this.rowLayoutId=rowLayoutId;
+    }
+
+    @Override
+    public View getNextPageView(View v, ViewGroup parent) {
+        if (v == null) {
+            // R.layout.adapter_next_page contains an ImageView with a custom graphic
+            // and a TextView.
+            v = View.inflate(getContext(), R.layout.next_page_list_item, null);
+        }
+        TextView textView = (TextView) v.findViewById(R.id.nextPageTextViewId);
+        final ProgressBar progressBar=(ProgressBar)v.findViewById(R.id.progress);
+        final ImageView arrowImage=(ImageView)v.findViewById(R.id.arrowImage);
+        progressBar.setVisibility(View.GONE);
+        arrowImage.setVisibility(View.VISIBLE);
+        textView.setText("SHOW MORE");
+        textView.setClickable(true);
+        final View finalV = v;
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View textView) {
+                ((TextView)textView).setText("LOADING MORE...");
+                progressBar.setVisibility(View.VISIBLE);
+                arrowImage.setVisibility(View.GONE);
+
+                finalV.performClick();
+
+            }
+        });
+
+        return v;
+    }
+
+    //Inflate della vista del singolo item
+    @Override
+    public View getItemView(final JobOffer jobOffer, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = View.inflate(getContext(), rowLayoutId, null);
+        }
+
+        TextView titleTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+        //TextView companyTextView = (TextView) convertView.findViewById(R.id.companyTextView);
+        TextView positionTextView = (TextView) convertView.findViewById(R.id.positionTextView);
+        TextView timeAgoTextView = (TextView) convertView.findViewById(R.id.timeAgoTextView);
+        TextView locationTextView=(TextView) convertView.findViewById(R.id.locationTextView);
+        final ImageButton innerButton =(ImageButton) convertView.findViewById(R.id.innerButton);
+
+        titleTextView.setText(jobOffer.getName());
+        positionTextView.setText(jobOffer.getContract());
+        //companyTextView.setText(jobOffer.getCompany().getName());
+        timeAgoTextView.setText((TimeManager.getFormattedDate(jobOffer.getCreatedAt())));
+        locationTextView.setText(jobOffer.getCompactLocation());
+
+        innerButtonManager.configureButton(jobOffer, innerButton);
+
+        return convertView;
+    }
+
+
+}
